510bc9d9c2deca977e9c9bcc6e6ced4e41544666
sharonpraju <47088116+sharonpraju@users.noreply.github.com>
2020-05-13 19:09:44 +0530

    Update register.php

--- ieee/register.php
+++ ieee/register.php
@@ -40,7 +40,7 @@ else
     {
         echo"<br><br><br><br><br><br><center>
             <div class='text-white'>
-            <h3>Submitted Successfully</h3>
+            <h2>Submitted Successfully</h2>
             You can also submit multiple problems.<br>Click the button below to submit another problem.
             <br>
             <a class='btn btn-info' href='pitchathon.php'>Submit Another Problem</a></div>
